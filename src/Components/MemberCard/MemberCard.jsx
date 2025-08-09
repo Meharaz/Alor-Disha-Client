8a1b5d746b510b12dd396150c2a25aee4ccb735c
Meharaz Hossain <freelancermeharaz@gmail.com>
2025-08-10 02:48:39 +0600

    Members filter

--- src/Components/MemberCard/MemberCard.jsx
+++ src/Components/MemberCard/MemberCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const MemberCard = ({item}) => {
-    const {name, age, gender, email, image, } = item; 
+    const {name, age, phone, email, image} = item; 
     return (
         <div>
             <h1>{name}</h1>
+            <p>{age}</p>
+            <p>{phone}</p>
+            <p>{email}</p>
+            <p>{image}</p>
         </div>
     );
 };
--- src/Pages/Members/Members.jsx
+++ src/Pages/Members/Members.jsx
@@ -3,37 +3,64 @@ import { Helmet } from 'react-helmet-async';
 import MemberCard from '../../Components/MemberCard/MemberCard';
 
 const Members = () => {
-  const [members, setMembers] = useState([]);
+    const [members, setMembers] = useState([]);
 
-  useEffect(() => {
-    fetch('members.json')
-      .then(res => res.json())
-      .then(data => {
-        if (data && data.members && Array.isArray(data.members)) {
-          setMembers(data.members);
-        } else {
-          console.error('Invalid data format:', data);
-          setMembers([]); // set empty array if data is not as expected
-        }
-      })
-      .catch(err => console.error('Fetch error:', err));
-  }, []);
+    useEffect(() => {
+        fetch('members.json')
+            .then(res => res.json())
+            .then(data => {
+                if (data && data.members && Array.isArray(data.members)) {
+                    setMembers(data.members);
+                } else {
+                    console.error('Invalid data format:', data);
+                    setMembers([]); // set empty array if data is not as expected
+                }
+            })
+            .catch(err => console.error('Fetch error:', err));
+    }, []);
 
-  return (
-    <div>
-      <Helmet>
-        <title>Alor Disha | Members</title>
-      </Helmet>
-      <h2>These are the honorable Members</h2>
-      <div>
-        {members && Array.isArray(members) && members
-        .filter(member => member.role === "Volunteer")
-        .map(item => (
-          <MemberCard key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Helmet>
+                <title>Alor Disha | Members</title>
+            </Helmet>
+            <h2>These are the honorable Members</h2>
+            <div className='grid gap-4 grid-cols-4'>
+                <div>
+                    <p className='text-xl font-bold'>Volunteer</p>
+                    {members && Array.isArray(members) && members
+                        .filter(member => member.role === "Volunteer")
+                        .map(item => (
+                            <MemberCard key={item.id} item={item} />
+                        ))}
+                </div>
+                <div>
+                <p className='text-xl font-bold'>Blood Doner</p>
+                    {members && Array.isArray(members) && members
+                        .filter(member => member.role === "Blood Donor")
+                        .map(item => (
+                            <MemberCard key={item.id} item={item} />
+                        ))}
+                </div>
+                <div>
+                <p className='text-xl font-bold'>Trusty Board Member</p>
+                    {members && Array.isArray(members) && members
+                        .filter(member => member.role === "Trusty Board Member")
+                        .map(item => (
+                            <MemberCard key={item.id} item={item} />
+                        ))}
+                </div>
+                <div>
+                <p className='text-xl font-bold'>General Member</p>
+                    {members && Array.isArray(members) && members
+                        .filter(member => member.role === "General Member")
+                        .map(item => (
+                            <MemberCard key={item.id} item={item} />
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Members;
